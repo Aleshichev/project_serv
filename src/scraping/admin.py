7e5332bc47b08c2972175560a45da7fa4a7ec753
Igor <Aleshichev.git@email.ua>
2022-05-17 13:07:39 +0300

    next commit

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import City, Language
+from .models import City, Language, Vacancy
 
 # Register your models here.
 
 admin.site.register(City)     # регистрация
 admin.site.register(Language)
+admin.site.register(Vacancy)
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -43,8 +43,17 @@ class Language(models.Model):
         super().save(*args, **kwargs)
 
 class Vacancy(models.Model):
-    url = models.URLField()
-    title = models.CharField(max_length=250, verbose_name='')
-    company = models.CharField(max_length=250, verbose_name='')
-    description = models.TextField(verbose_name='')
-    city = models.ForeignKey('City', )
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансий')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город' )
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования' )
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансия'
+
+    def __str__(self):
+        return self.title
